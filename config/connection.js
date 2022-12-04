66cae8531d5d21db2b7dc22452949081d22f4715
Gavin <gsantopetro.contact@gmail.com>
2022-12-04 14:29:29 -0600

    added file structure, config

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mgs = require('mongoose');
+
+
+mgs.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = mgs.connection;
